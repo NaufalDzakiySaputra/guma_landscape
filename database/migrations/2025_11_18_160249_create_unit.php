6507a8c85c5b90cb1d0e6f1d14a9d3b9d6013357
Joanna Mareta <joannamareta07@gmail.com>
2025-11-20 09:39:23 +0700

    tambah table unit

--- database/migrations/2025_11_18_160249_create_unit.php
+++ database/migrations/2025_11_18_160249_create_unit.php
@@ -14,11 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('unit', function (Blueprint $table) {
-        $table->id('id_unit');
+       $table->id('id_unit');
         $table->string('nama_unit');
-        $table->integer('harga_unit');
-        $table->integer('kapasitas');
-        $table->enum('status_unit', ['available', 'booked'])->default('available');
+        $table->integer('harga_weekday');
+        $table->integer('harga_weekend');
+        $table->integer('harga_libur');
+        $table->integer('harga_libur_besar')->nullable();
+        $table->integer('kapasitas')->default(1);
+        $table->enum('status_unit', ['tersedia', 'dipesan', 'maintenance'])->default('tersedia');
         $table->text('deskripsi_unit')->nullable();
         $table->timestamps();
         });
